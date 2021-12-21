1d83a8c641936d5757ebcfb7836e5d3b40d36d61
Richmond Efa Oduro <richmond.eo@gmail.com>
2021-12-21 14:33:48 +0100

    Header fix

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,28 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue"
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    Header
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #f4f8fd;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,43 @@
 <template>
   <nav>
-    <router-link to="form">Form</router-link>
-    <router-link to="result">Result</router-link>
+    <router-link class="routerlink" active-link="active" to="form"
+      >Form</router-link
+    >
+    <router-link class="routerlink" active-link="active" to="result"
+      >Result</router-link
+    >
   </nav>
 </template>
 
 <script>
-
 export default {
-  name: 'Header'
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Mulish:wght@200&display=swap");
+
+nav {
+  background-color: #fff;
+  top: 0;
+  display: flex;
+  height: 85px;
+  left: 0;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 30px;
+}
+
+.routerlink {
+  font-family: "Mulish", "san-serif";
+  color: black;
+  font-weight: bold;
+  padding: 10px;
+  text-decoration: none;
+}
+
+.router-link-active {
+  text-decoration: underline;
+}
 </style>

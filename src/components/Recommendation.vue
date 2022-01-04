ed08bf2d3c971a2474f3541af1d057eaa1db61f9
Martina Jonsson <martina.jonsson@knowit.se>
2022-01-04 10:00:14 +0100

    Added sources

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -2,11 +2,11 @@
   <p>
     To add some more relevancy to these numbers, we may compare them with some
     general stats. As seen in the above diagram, the average transportation
-    related CO2-emissions per year and person was 1.7 tons 2018. Far from the
+    related CO2-emissions per year and person was 1.7 tons 2019 (Naturvårdsverket 2019). Far from the
     goal of 1 ton per person and year in average global emissions (also seen in
     diagram) by the year of 2050, in order to achieve the
     <i>Generation Goal</i>, the environmental quality goal
-    <i>Limited Climate Impact</i> and the <i>Paris Agreement goal</i>.
+    <i>Limited Climate Impact</i> and the <i>Paris Agreement goal</i> (Sveriges Miljömål 2018).
   </p>
   <p>
     Going by car to the selected destination results in
@@ -64,6 +64,11 @@
     % of the average amount of global emissions per year and person to achieve
     the above environmental goals (1 ton).
   </p>
+  <h3>Referenslista</h3>
+  <ol>
+    <li>Sveriges Miljömål (2018) <i>Konsumtionsbaserade utsläpp av växthusgaser per område</i> sverigesmiljomal.se/miljomalen/generationsmalet/konsumtionsbaserade-vaxthusgasutslapp-per-omrade/ [2021-12-30]</li>
+    <li>Naturvårdsverket (2019) <i>Konsumtionsbaserade växthusgasutsläpp per person och år</i> https://www.naturvardsverket.se/data-och-statistik/konsumtion/vaxthusgaser-konsumtionsbaserade-utslapp-per-person/ [2021-12-30]</li>
+  </ol>
 </template>
 <script>
 export default {
